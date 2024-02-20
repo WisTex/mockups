764b522c6f32d6c586c0b31761e48755b0e34e51
Scott M. Stolz <sstolz@wistex.com>
2024-02-19 18:15:20 -0600

    Scenes

---
+++ geoinsights/i.php
@@ -0,0 +1,82 @@
+      <?php include 'include/header.php'; ?>
+        <!-- Page body -->
+        <div class="page-body">
+          <div class="container-xl">
+            <div class="row row-cards">
+              <div class="col-lg-9">
+
+
+
+              <div class="card mb-3" style="min-height: 500px;">
+                  <!--
+                  <div class="card-header">
+                    <h3 class="card-title">Card title <span class="card-subtitle">Subtitle</span></h3>
+                  </div>
+                  -->
+                  <div class="card-body">
+                    <h3 class="card-title mb-2">Stacked Seismic Volume <span class="card-subtitle">Channel and Fan</span></h3> 
+                    <p><img src="images/3D-channel-and-fans.jpg"></img></p>
+                  </div>
+                  <div class="card-footer">
+                  <a href="#" class="btn btn-primary btn-sm">Back to Dashboard</a>
+                  <a href="#" class="btn btn-primary btn-sm">View Full Screen    &nbsp;
+                  <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-external-link" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 6h-6a2 2 0 0 0 -2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-6" /><path d="M11 13l9 -9" /><path d="M15 4h5v5" /></svg>
+
+                  </a>                  
+                  <a href="#" class="btn btn-warning btn-sm">Change Data Set</a>
+                  </div>
+
+                </div>
+
+
+
+              </div>
+              
+              
+              <div class="col-lg-3">
+
+
+
+
+
+              <div class="card mb-3">
+                  <!-- 
+                  <div class="card-header">
+                    <h3 class="card-title">View Dataset <span class="card-subtitle">Application</span></h3>
+                  </div>
+                  -->
+                  <div class="card-body">
+                    <h3 class="card-title mb-2">Select Actions</h3>
+                    
+                    
+                    <a href="#" class="btn btn-primary btn-sm mb-1">Compute Geobodies</a>
+                    
+                    
+                    
+
+                  </div>
+
+                  <div class="card-footer">
+                  <h3 class="card-title mb-2">More Options</h3>
+                    <a href="#" class="btn btn-primary btn-sm mb-1">App Builder</a>  
+                    <a href="#" class="btn btn-primary btn-sm mb-1">Advanced Options</a>
+                    <a href="#" class="btn btn-primary btn-sm mb-1">View History</a>
+                    <a href="#" class="btn btn-warning btn-sm mb-1">Change Application</a>
+                  </div>                  
+
+
+                </div>
+                
+                
+
+
+                
+
+              </div>
+
+
+              
+            </div>
+          </div>
+        </div>
+      <?php include 'include/footer.php'; ?>
