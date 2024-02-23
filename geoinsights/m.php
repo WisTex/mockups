80c374d607a77b55a1b9b6cc614331aa5b5af3b1
Scott M. Stolz <sstolz@wistex.com>
2024-02-23 12:59:26 -0600

    Applications

---
+++ geoinsights/m.php
@@ -0,0 +1,145 @@
+      <?php include 'include/header.php'; ?>
+        <!-- Page body -->
+        <div class="page-body">
+          <div class="container-xl">
+            <div class="row row-cards">
+              <div class="col-lg-4">
+
+                <div class="card mb-3">
+                  <div class="card-body">
+                    <div class="d-flex align-items-center mb-3">
+                      <div class="me-3">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-md icon-tabler icon-tabler-map-search" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M11 18l-2 -1l-6 3v-13l6 -3l6 3l6 -3v7.5" /><path d="M9 4v13" /><path d="M15 7v5" /><path d="M18 18m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" /><path d="M20.2 20.2l1.8 1.8" /></svg>
+                      </div>
+                      <div>
+                        <small class="text-secondary">Location</small>
+                        <h3 class="lh-1">Gulf of Mexico</h3>
+                      </div>
+                    </div>
+                    <p><img src="images/Gulf-of-Mexico-Map.png"></img></p>
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                  </div>
+<!--
+                  <div class="ribbon bg-green">
+                  Selected
+                  </div>
+    -->
+                  <div class="card-footer">
+                    <a href="map.php" class="btn btn-primary btn-sm">View Map</a>
+                    
+                  </div>
+                </div>
+
+                
+
+
+                <div class="card mb-3">
+                  <!--
+                  <div class="card-header">
+                    <h3 class="card-title">Card title <span class="card-subtitle">Subtitle</span></h3>
+                  </div>
+                  -->
+                  <div class="card-body">
+                    <h3 class="card-title mb-2">Progress <!-- <span class="card-subtitle">Processing in Cloud</span> -->
+                    <span class="status status-cyan pull-right">
+                      <span class="status-dot status-dot-animated"></span>
+                        Processing in Cloud
+                      </span>
+                    </h3>
+                    <div class="progress progress-lg mt-3 mb-3">
+                      <div class="progress-bar progress-bar-indeterminate"></div>
+                    </div>
+                    <a href="h.php" class="btn btn-primary disabled btn-sm">Processing...</a>  
+                  </div>
+                </div>  
+
+
+
+
+              
+
+                <div class="card mb-3">
+                  <!--
+                  <div class="card-header">
+                    <h3 class="card-title">Card title <span class="card-subtitle">Subtitle</span></h3>
+                  </div>
+                -->
+
+                  <div class="card-body">
+                    <ul class="steps steps-counter steps-vertical">
+                      <li class="step-item">Select Location: Gulf of Mexico</li>
+                      <li class="step-item">Select Data Set: Stacked Seismic Volume</li>
+                      <li class="step-item active">Select Application: View Dataset</li>
+                      <li class="step-item">Select Models &amp; Recipes</li>
+                      <li class="step-item">Select Parameters</li>
+                      <li class="step-item">View Result</li>
+                    </ul>
+                  </div>
+                </div>
+
+
+                <div class="card mb-3">
+                  <!--
+                  <div class="card-header">
+                    <h3 class="card-title">Card title <span class="card-subtitle">Subtitle</span></h3>
+                  </div>
+                -->
+
+                  <div class="card-body">
+                    <ul class="steps steps-counter steps-vertical">
+                      <li class="step-item">Select Location: Gulf of Mexico</li>
+                      <li class="step-item">Select Data Set: Stacked Seismic Volume</li>
+                      <li class="step-item">Select Application: View Dataset</li>
+                      <li class="step-item">Select Parameters: Amplitude Scale Change</li>
+                      <li class="step-item active">View Result</li>
+                    </ul>
+                  </div>
+                </div>
+
+
+
+              </div>
+              
+              
+              <div class="col-lg-8">
+
+
+
+
+
+                <div class="card mb-3" style="min-height: 800px;">
+                  <!--
+                  <div class="card-header">
+                    <h3 class="card-title">Card title <span class="card-subtitle">Subtitle</span></h3>
+                  </div>
+                  -->
+                  <div class="card-body">
+                    <h3 class="card-title mb-2">Applications <span class="card-subtitle">Coming soon!</span></h3> 
+                    
+                  </div>
+                  <div class="card-footer">
+
+                  <a href="h.php" class="btn btn-warning btn-sm">Next</a>
+                  </div>
+
+                </div>
+                
+                
+
+
+                
+
+              </div>
+
+
+              
+            </div>
+          </div>
+        </div>
+      <?php include 'include/footer.php'; ?>
