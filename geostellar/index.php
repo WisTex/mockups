71b3e8c78d37f702e4a59ba0369b3edca673f49f
Scott M. Stolz <sstolz@wistex.com>
2024-04-08 16:09:12 -0500

    Update index.php

--- geostellar/index.php
+++ geostellar/index.php
@@ -2,12 +2,12 @@
 <html>
     <head>
         <title>Stellarsolve Energy</title>
-
+        
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        
         <link rel="stylesheet" href="./view/theme/neuhub-tab/css/codestitch.css">
         <link rel="stylesheet" href="./view/theme/neuhub-tab/css/codestitch-sections.css">
-
+        
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     </head>
     <body>
@@ -122,8 +122,6 @@
             </div>
         </header>
        
-        <script src="./view/theme/neuhub-tab/js/codestitch.js"></script>
-
 <!-- ============================================ -->
 <!--                    Hero                      -->
 <!-- ============================================ -->
@@ -287,7 +285,7 @@
 </section>
                                 
 
-
+<script src="./view/theme/neuhub-tab/js/codestitch.js"></script>
 
     </body>
 </html>
